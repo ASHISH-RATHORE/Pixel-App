6d8e41ab375a7566e228b6828f4f664f7ac1f9a2
ashish <ashishr97@gmail.com>
2021-02-24 15:21:37 +0530

    second

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ const globalErrorHandler = require('./Authentication/errorController');
 const userRouter=require('./Routes/userRoutes');
 const imgController = require('./controllers/imageController');
 const cookie=require('cookie-parser');
+const cors=require('cors');
+const compression=require('compression');
+
+
 
 const app=express();
+
+app.use(cors());
 
 app.use(cookie());
 app.use(express.json());
 
 
-
+app.use(compression())
 
 // Routes for pixel
 app.use('/api/v1/img',imgRouter);
@@ -23,8 +29,7 @@ app.all('*',(req,res,next)=>{
     return next(new AppError(`can't find ${req.originalUrl} on this Server`,404));
 });
 
-// app.use(globalErrorHandler);
-
+app.use(globalErrorHandler);
 
 
 module.exports=app;
